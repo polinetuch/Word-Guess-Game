556627d844487b92f50d5545dc8d387670d16e1a
unknown <uchpolinet@gmail.com>
2019-07-05 20:56:28 +1000

    create variables to display text in HTML webpage and assign each element using .textContent

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,32 @@
-var computerChoices = ["Jon Snow", "Khaleesi", "Tyrion", "Ser Jamie", "Ser Davos"]
+// Assign an array of names that computer can choose from
+var names = ["Jon Snow", 
+"Khaleesi", 
+"Tyrion", 
+"Ser Jamie", 
+"Ser Davos",
+"Lady Sanasa",
+"Sam Tarley"]
+
+// Assign variables to each counter
+var randomNames = [];
+var wrongGuess = 0;
+var guessesLeft = 12;
+var wins = 0;
+var losses = 0;
+
+// Create variables that reference the HTML ids to display the text.
+var randomNames = document.getElementById("randomNamesText");
+var wrongGuess = document.getElementById("wrongGuessesText");
+var guessesLeft = document.getElementById("guessesLeftText");
+var wins = document.getElementById("winsText");
+var losses = document.getElementById("lossesText");
+
+// To load the HTML page elements assign in the above
+randomNamesText = "randomNamesText" + randomNames;
+wrongGuessText.textContent = "wrongGuessText" + wrongGuess;
+guessesleftText.textContent = "guessesLeftText" + guessesLeft;
+winsText.textContent = "winsText" + wins;
+lossesText.textContent = "lossesText" + losses;
+
+// assign the computer's guess by picking a random name from the array
+var computerChoice = names[Math.floor(Math.random() * names.length)];
