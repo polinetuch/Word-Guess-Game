e1bacffcd031c9b446844a94f239a796bbbd75ad
unknown <uchpolinet@gmail.com>
2019-07-06 19:25:41 +1000

    create function to add space to each index of a string using split and join, assign a user key event  using boolean, use charAt to identify each letter (index) of the word (string)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,6 +29,8 @@ var winsNumber = 0;
 var lossesNumber = 0;
 // a string that stores "_" values, and is the same length as the computer choice
 var letterGuessedRight = "";
+// a version of letterGuessedRight to display on screen
+var formattedLetterGuessedRight = "";
 
 var computerChoice = names[Math.floor(Math.random() * names.length)];
 console.log(computerChoice);
@@ -41,35 +43,55 @@ var winsText = document.getElementById("winsText");
 var lossesText = document.getElementById("lossesText");
 
 // to display the random computerChoice in blank _ _ _ _
-function assignedLetterGuessedRightToBlank(str) {
-    var letterCount = str.length;
+function assignedLetterGuessedRightToBlank() {
+    var letterCount = computerChoice.length;
     
     // do a loop for the length of letter count in the str
     // during each loop, add a "_" to the value letterGuessedRight
     // 1ST letterGuessedRight = "_"
     // 2nd letterGuessedRight = "__"
     // 3rd letterGuessedRight = "___"
-    for ( var i = 0; i < str.length; i++) {
-        letterGuessedRight = letterGuessedRight + "_ ";
-    }
+    for ( var i = 0; i < computerChoice.length; i++) {
+        letterGuessedRight = letterGuessedRight + "_";
+    };
     console.log(letterGuessedRight.length);
 }
 
 assignedLetterGuessedRightToBlank(computerChoice);
 
+// format letterGuessedRight to have spaces
+function addSpacesBetweenLetters() {
+    formattedLetterGuessedRight = letterGuessedRight.split('').join(' ');
+}
+
+addSpacesBetweenLetters();
 
 // To load the HTML page elements assign in the above
-correctGuessText.textContent = "The Game of Thrones character is " + letterGuessedRight;
+correctGuessText.textContent = "The Game of Thrones character is " + formattedLetterGuessedRight;
 wrongGuessText.textContent = "Wrong Guesses: " + wrongGuessNumber;
 guessesLeftText.textContent = "Guesses Left: " + guessesLeftNumber;
 winsText.textContent = "Wins: " + winsNumber;
 lossesText.textContent = "Losses: " + lossesNumber;
 
-
+// grab that is pressed by the user
 document.onkeyup = function(event) {
+    // when the key is triggerd
     var userGuess = event.key;
-} if (userGuess === letterGuessedRight) {
-    
+
+    // a boolean variable that says if the user guessed correctly or not
+    var userGuessCorrectLetter = false;
+    // loop through each letter in the computer choice word
+    for (var i = 0; i < computerChoice.length; i++) {
+        // check if the letter matches the user guess
+        if (userGuess === computerChoice.charAt(i)) {
+            // if it equals, substitute the letter index in letterGuessedRight with the guessed letter
+            letterGuessedRight.charAt(i) = computerChoice.charAt(i);
+            // set the boolean variable to true
+            userGuessCorrectLetter = true;
+
+        }
+    } 
+
 }
 
 
