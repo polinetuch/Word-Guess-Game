8119991ca691fde4e53379be24d169fc187ab276
unknown <uchpolinet@gmail.com>
2019-07-06 21:09:27 +1000

    create function for resetGame, resetScreen - add if statement to check for correct/incorrect guesses and add the value to the counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,11 @@
 // Assign an array of names that computer can choose from
-var names = ["Jon", 
-"Khaleesi", 
-"Tyrion", 
-"Jamie", 
-"Davos",
-"Sansa",
-"Samwell"]
-
-
+var names = ["jon", 
+"khaleesi", 
+"tyrion", 
+"jamie", 
+"davos",
+"sansa",
+"samwell"]
 
 //computer picked== tyroyn
 // computer to select a random name from the array
@@ -17,20 +15,16 @@ var names = ["Jon",
 // show the dashArray
 // user presses a key and it pushes to the string of the item from the name
 
-
-
-
-
-
 // Assign variables to each counter
-var wrongGuessNumber = 0;
 var guessesLeftNumber = 12;
+var correctGuessCount = 0;
 var winsNumber = 0;
 var lossesNumber = 0;
-// a string that stores "_" values, and is the same length as the computer choice
-var letterGuessedRight = "";
+// a string that stores guessed values, and is the same length as the computer choice
+var letterGuessedRight = [];
 // a version of letterGuessedRight to display on screen
 var formattedLetterGuessedRight = "";
+var lettersGuessedWrong = [];
 
 var computerChoice = names[Math.floor(Math.random() * names.length)];
 console.log(computerChoice);
@@ -44,6 +38,8 @@ var lossesText = document.getElementById("lossesText");
 
 // to display the random computerChoice in blank _ _ _ _
 function assignedLetterGuessedRightToBlank() {
+    letterGuessedRight = [];
+    
     var letterCount = computerChoice.length;
     
     // do a loop for the length of letter count in the str
@@ -52,26 +48,44 @@ function assignedLetterGuessedRightToBlank() {
     // 2nd letterGuessedRight = "__"
     // 3rd letterGuessedRight = "___"
     for ( var i = 0; i < computerChoice.length; i++) {
-        letterGuessedRight = letterGuessedRight + "_";
+        letterGuessedRight.push("_");
     };
     console.log(letterGuessedRight.length);
 }
 
 assignedLetterGuessedRightToBlank(computerChoice);
 
-// format letterGuessedRight to have spaces
-function addSpacesBetweenLetters() {
-    formattedLetterGuessedRight = letterGuessedRight.split('').join(' ');
+// format letterGuessedRight to a string that has spaces
+function letterGuessedRightFormatAsString() {
+    //convert the array to a string
+    formattedLetterGuessedRight = letterGuessedRight.join();
+    // add spaces
+    formattedLetterGuessedRight = formattedLetterGuessedRight.split(',').join(' ');
 }
 
-addSpacesBetweenLetters();
+// reset the game - reset counters and increases the value of lossesNumber
+function resetGame() {
+    computerChoice =names[Math.floor(Math.random() * names.length)];
+    console.log(computerChoice);
+    guessesLeftNumber = 12;
+    assignedLetterGuessedRightToBlank();
+    lettersGuessedWrong = [];
+}
 
-// To load the HTML page elements assign in the above
-correctGuessText.textContent = "The Game of Thrones character is " + formattedLetterGuessedRight;
-wrongGuessText.textContent = "Wrong Guesses: " + wrongGuessNumber;
-guessesLeftText.textContent = "Guesses Left: " + guessesLeftNumber;
-winsText.textContent = "Wins: " + winsNumber;
-lossesText.textContent = "Losses: " + lossesNumber;
+// reset the screen with updated values
+function resetScreen() {
+    // reformat the letters gussed string
+    letterGuessedRightFormatAsString();
+
+    // To load the HTML page elements assign in the above   
+    correctGuessText.textContent = "The Game of Thrones character is " + formattedLetterGuessedRight;
+    wrongGuessText.textContent = "Wrong Guesses: " + lettersGuessedWrong.join();
+    guessesLeftText.textContent = "Guesses Left: " + guessesLeftNumber;
+    winsText.textContent = "Wins: " + winsNumber;
+    lossesText.textContent = "Losses: " + lossesNumber;
+}
+
+resetScreen();
 
 // grab that is pressed by the user
 document.onkeyup = function(event) {
@@ -79,20 +93,42 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
 
     // a boolean variable that says if the user guessed correctly or not
-    var userGuessCorrectLetter = false;
+    var userGuessedCorrectLetter = false;
+
     // loop through each letter in the computer choice word
     for (var i = 0; i < computerChoice.length; i++) {
         // check if the letter matches the user guess
         if (userGuess === computerChoice.charAt(i)) {
             // if it equals, substitute the letter index in letterGuessedRight with the guessed letter
-            letterGuessedRight.charAt(i) = computerChoice.charAt(i);
+            letterGuessedRight[i] = computerChoice.charAt(i);
+            correctGuessCount++;
             // set the boolean variable to true
-            userGuessCorrectLetter = true;
+            userGuessedCorrectLetter = true;
+        } 
+    }
 
-        }
-    } 
+    // after looping through each letter, check if the user made a correct guess
+    if (userGuessedCorrectLetter === false) {
+        // add the user guess to the lettersGuessedWrong array
+        lettersGuessedWrong.push(userGuess);
+        guessesLeftNumber = guessesLeftNumber - 1;
+    }
+    
+    // check if the user has won
+    if (correctGuessCount === computerChoice.length) {
+        winsNumber++;
+        alert("Congraluations! You've won! The word was " + computerChoice);
+        resetGame();
+    }
+    // Check if the user has lost based on guesses left
+    else if (guessesLeftNumber === 0) {
+        lossesNumber++;
+        alert("Sorry, try again later!")
+        resetGame();
+    }
 
-}
+    resetScreen();
+} 
 
 
 
